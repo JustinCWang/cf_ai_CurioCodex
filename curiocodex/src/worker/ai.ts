74f16c5b9f0aca656c0a089597495d7ecfd8bcf5
Justin Wang <jwlion14@gmail.com>
2025-11-17 17:52:45 -0500

    Added Digital Media to the Categories

--- curiocodex/src/worker/ai.ts
+++ curiocodex/src/worker/ai.ts
@@ -56,6 +56,7 @@ export async function categorizeItem(
   try {
     const categories = [
       "Arts & Crafts",
+      "Digital Media",
       "Technology",
       "Sports & Fitness",
       "Music",
